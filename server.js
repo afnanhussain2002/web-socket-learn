eff5610396efe763a203bdb500e86acce1fa230f
afnanhussain2002 <afnanhussain2022@gmail.com>
2026-06-09 12:31:30 +0600

    make a port for run

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import http from 'node:http';
 
+const PORT = process.env.PORT || 9000
 
 const httpServer = http.createServer(async (req, res) => {
     
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server started on http://localhost: ${PORT}`)
 })
